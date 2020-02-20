f26a992d7942cad3f3a3d81d1054bb8337b371d4
Marcos <marcos@workspoke.com>
2020-02-19 21:45:31 -0400

    sidenav css

--- components/Sections/SideNav.vue
+++ components/Sections/SideNav.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
   .side-nav-content
-    .side-nav-backdrop(v-if="open")
-    .side-nav(v-if="open")
-      LangSwitcher
-      ul.nav-list
-        li.nav-item
-          nuxt-link(to="/blog") {{ $t('navItems.blog') }}
+    .side-nav-backdrop(v-if="open" @click="toggleSidenav")
+    transition(name="slide-left")
+      .side-nav(v-if="open")
+        LangSwitcher
+        ul.nav-list
+          li.nav-item
+            nuxt-link(:to="localePath('blog')") {{ $t('navItems.blog') }}
 </template>
 <script lang="js">
 import LangSwitcher from '~/components/LangSwitcher'
@@ -24,9 +25,36 @@ export default {
     open() {
       return this.$store.state.openSidenav
     }
+  },
+  methods:{
+    toggleSidenav() {
+      return this.$store.commit('toggleSidenav')
+    }
   }
 }
 </script>
 <style lang="scss">
+.side-nav-content{
+  height: 100%;
+  width: 100%;
 
+  .side-nav-backdrop {
+    position: fixed;
+    background-color: #030303;
+    top: 0;
+    opacity: 0.4;
+    width: 100%;
+    height: 100%;
+  }
+  .side-nav {
+    position: fixed;
+    background-color: #08a8f2;
+    top: 0;
+    width: 220px;
+    height: 100%;
+    a{
+      color: white;
+    }
+  }
+}
 </style>
